c12ea697047f7b217424de11dc53730bde5ebc85
Lloyd Pick <lloydpick@gmail.com>
2018-07-20 11:59:34 -0400

    look for all Gemfile.locks
    not just the one in the root

--- garrison/checks/check_bundler_audit.rb
+++ garrison/checks/check_bundler_audit.rb
@@ -29,18 +29,32 @@ module Garrison
             Logging.info "Found #{repositories.count} repositories"
             repositories.reject! { |repo| options[:github_exclude_repositories].include?(repo.full_name) } if options[:github_exclude_repositories]
             Logging.info "Found #{repositories.count} repositories after exclusions"
+
             repositories.each do |repo|
 
-              Logging.info "#{repo.full_name} - Looking for Gemfile.lock"
-              Dir.mktmpdir(repo.full_name.tr('/', '-')) do |dir|
-                begin
-                  gemfile = org.contents(repo.full_name, path: 'Gemfile.lock')
-                  File.open(File.join(dir, 'Gemfile.lock'), 'w') { |f| f.write(Base64.decode64(gemfile[:content])) }
-                  Logging.info "#{repo.full_name} - Scanning"
-                  scan_lock_file(repo, dir)
-                rescue Octokit::NotFound
-                  Logging.info "#{repo.full_name} - Skipping - Gemfile.lock not found"
-                  next
+              Logging.info "#{repo.full_name} - Looking for Gemfile.lock files"
+              begin
+                search = org.search_code("filename:Gemfile.lock repo:#{repo.full_name}")
+              rescue Octokit::TooManyRequests
+                Logging.warn "Octokit::TooManyRequests - Sleeping for #{org.rate_limit.resets_in}s"
+                sleep org.rate_limit.resets_in + 1
+                Logging.info 'Retrying request'
+                retry
+              end
+              Logging.info "#{repo.full_name} - #{search[:total_count]} found"
+              next if search[:total_count].zero?
+
+              search[:items].each do |gemfile|
+                Dir.mktmpdir(repo.full_name.tr('/', '-')) do |dir|
+                  begin
+                    gemfile = org.contents(repo.full_name, path: gemfile.path)
+                    File.open(File.join(dir, 'Gemfile.lock'), 'w') { |f| f.write(Base64.decode64(gemfile[:content])) }
+                    Logging.info "#{repo.full_name} - Scanning #{gemfile.path}"
+                    scan_lock_file(repo, dir, gemfile)
+                  rescue Octokit::NotFound
+                    Logging.info "#{repo.full_name} - Skipping - Gemfile.lock not found"
+                    next
+                  end
                 end
               end
 
@@ -51,7 +65,7 @@ module Garrison
 
       private
 
-      def scan_lock_file(repo, dir)
+      def scan_lock_file(repo, dir, gemfile)
         scanner = Bundler::Audit::Scanner.new(dir)
         scanner.scan.each do |result|
 
@@ -61,16 +75,20 @@ module Garrison
           when Bundler::Audit::Scanner::UnpatchedGem
             alert(
               name: 'Vulnerable Dependency',
-              target: repo.full_name,
+              target: "#{repo.full_name}/#{gemfile.path}",
               detail: "#{result.gem.name}: #{result.advisory.id}",
               finding: result.advisory.to_h.to_json,
               note: result.advisory.description,
               external_severity: CveHelper.cvss_score_to_severity(result.advisory.cvss_v2),
-              finding_id: "#{repo.full_name}-#{result.gem.name}-#{result.advisory.id}",
+              finding_id: "#{repo.full_name}-#{gemfile.path}-#{result.gem.name}-#{result.advisory.id}",
               urls: [
                 {
                   name: 'Github Repository',
                   url: repo.html_url
+                },
+                {
+                  name: 'Github Repository - Gemfile.lock',
+                  url: gemfile.html_url
                 },
                 {
                   name: 'CVE Details',
